172c83b7d615b9c4a5fb50ba80417402c43ec016
SuperActionMaker <54882490+SuperActionMaker@users.noreply.github.com>
2019-09-29 10:53:29 +0900

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,5 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>初めてのPHPプログラミング</title>
+</head>
+<body>
 <?php
 $hoge1 = $_POST['hiragana'];
 $hoge2 = $_POST['katakana'];
 echo $hoge1 . $hoge2;
 ?>
+<body>
+<html>
